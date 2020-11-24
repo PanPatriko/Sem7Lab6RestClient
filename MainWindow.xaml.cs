65168447de9e602449fc3f131e509d3519f5f2bc
Patryk Panasiuk <ppanasiuk58@gmail.com>
2020-11-24 08:21:32 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿//using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab6RestClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HttpClient client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            client = new HttpClient();          
+            client.BaseAddress = new Uri("http://localhost:57667/");
+        }
+        private async void GetPeople()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var streamTask = client.GetStreamAsync("api/people");
+            var people = await JsonSerializer.DeserializeAsync<List<Person>>(await streamTask);
+            PeopleListBox.ItemsSource = people;
+        }
+
+        private void GetPeopleButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                GetPeople();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void GetPersonButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var streamTask = client.GetStreamAsync("api/people/"+IdTB.Text);
+                var person = await JsonSerializer.DeserializeAsync<Person>(await streamTask);
+                List<Person> people = new List<Person> { person };
+                PeopleListBox.ItemsSource = people;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void DelPersonButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (PeopleListBox.SelectedItem != null)
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var responseMessage = await client.DeleteAsync("api/people/" + (PeopleListBox.SelectedItem as Person).Id);
+                    if (responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        MessageBox.Show("Pomyślnie usunięto " + (PeopleListBox.SelectedItem as Person).ToString(), "OK", MessageBoxButton.OK, MessageBoxImage.Information);
+                        GetPeople();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void AddPersonButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(int.TryParse(YearTB.Text, out int n))
+                {
+                        client.DefaultRequestHeaders.Accept.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                        Person person = new Person();
+                        person.Name = NameTB.Text;
+                        person.Surname = SurnameTB.Text;
+                        person.City = CityTB.Text;
+                        person.Year = n;
+
+                        var json = JsonSerializer.Serialize(person);
+                        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                        var responseMessage = await client.PostAsync("api/people", content);
+                        if (responseMessage.StatusCode == System.Net.HttpStatusCode.Created)
+                        {
+                            MessageBox.Show("Pomyślnie dodano " + person.ToString(), "OK", MessageBoxButton.OK, MessageBoxImage.Information);
+                            GetPeople();
+                        }                   
+                }
+                else
+                {                     
+                    MessageBox.Show("Niepoprawna wartość w polu Rok","",MessageBoxButton.OK,MessageBoxImage.Error);                    
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void EditPersonButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (int.TryParse(YearTB.Text, out int n))
+                {
+                    if(PeopleListBox.SelectedItem != null)
+                    {
+                        client.DefaultRequestHeaders.Accept.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                        Person person = new Person();
+                        person.Name = NameTB.Text;
+                        person.Surname = SurnameTB.Text;
+                        person.City = CityTB.Text;
+                        person.Year = n;
+
+                        var json = JsonSerializer.Serialize(person);
+                        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                        var responseMessage = await client.PutAsync("api/people/" + (PeopleListBox.SelectedItem as Person).Id, content);
+                        if (responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            MessageBox.Show("Pomyślnie zmieniono " + (PeopleListBox.SelectedItem as Person).ToString(), "OK", MessageBoxButton.OK, MessageBoxImage.Information);
+                            GetPeople();
+                        }
+                    }    
+                }
+                else
+                {
+                    MessageBox.Show("Niepoprawna wartość w polu Rok", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Lab6RestClient
+{
+    class Person
+    {
+        [JsonPropertyName("Id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("Surname")]
+        public string Surname { get; set; }
+
+        [JsonPropertyName("City")]
+        public string City { get; set; }
+
+        [JsonPropertyName("Year")]
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " " + Surname;
+        }
+    }
+}
